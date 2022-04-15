9b237c94e63db7be0a907cd7fd9528cd17e1f003
tomaslami <tomaslamiguralnik@gmail.com>
2022-04-15 18:56:45 -0300

    ErrorEnGenerarCambios

--- Assets/Scripts/valicontra.cs
+++ Assets/Scripts/valicontra.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class valicontra : MonoBehaviour
 {
+    string contracorrecta;
+    string contraUsr;
+    public Text ingresoUsr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        contracorrecta = "1234";
     }
+    
 
     // Update is called once per frame
     void Update(){
          }
+
     public void valiContra()
     {
-
+        contraUsr = ingresoUsr.text;
+        if (contraUsr == contracorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else if (contraUsr != contracorrecta)
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 }
